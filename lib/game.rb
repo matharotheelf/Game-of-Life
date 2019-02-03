19bb2582f05106940db160242bb9791d11dde5b5
matharotheelf <mrthomasjacoblawrence@hotmail.co.uk>
2019-02-03 22:23:45 +0000

    Feature: Added nextiterationlivecell? method returns correct result if not livecell and no neighbours

--- lib/game.rb
+++ lib/game.rb
@@ -1,4 +1,7 @@
 require_relative 'grid.rb'
 
 class Game
+  def nextiterationlivecell?(_celldetails)
+    false
+  end
 end
--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -1,4 +1,13 @@
 require 'game'
 
 describe Game do
+  before do
+    @game = Game.new
+  end
+
+  describe '#nextiterationlivecell?' do
+    it 'returns false if not live cell and no neighbours' do
+      expect(@game.nextiterationlivecell?([0, false])).to eq(false)
+    end
+  end
 end
