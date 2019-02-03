b35ad27d62b43a1715a528d4b082b9700cecb498
matharotheelf <mrthomasjacoblawrence@hotmail.co.uk>
2019-02-03 19:33:44 +0000

    Feature: Added method which creates hash of relevent coordinates and their status for next iteration

--- spec/grid_spec.rb
+++ spec/grid_spec.rb
@@ -102,6 +102,8 @@ describe Grid do
 
   describe '#livecellneighbournumberhash' do
     let(:livecell1) { double :livecell1 }
+    let(:livecell2) { double :livecell2 }
+
     it 'creates hash for single live cell' do
       allow(livecell1).to receive(:coordinate) { [0, 0] }
       @grid.addlivecell(nil, nil, livecell1)
@@ -115,5 +117,28 @@ describe Grid do
                                                            [1, 0] => [1, false],
                                                            [1, 1] => [1, false])
     end
+
+    # it 'creates hash for multiple live cells' do
+    #   allow(livecell1).to receive(:coordinate) { [0, 0] }
+    #   @grid.addlivecell(nil, nil, livecell1)
+    #   allow(livecell2).to receive(:coordinate) { [1, 1] }
+    #   @grid.addlivecell(nil, nil, livecell2)
+    #   expect(@grid.livecellneighbournumberhash).to include([-1, -1] => [1, false],
+    #                                                        [-1, 0] => [1, false],
+    #                                                        [-1, 1] => [1, false],
+    #                                                        [-1, 2] => [0, false],
+    #                                                        [0, -1] => [1, false],
+    #                                                        [0, 0] => [1, true],
+    #                                                        [0, 1] => [2, false],
+    #                                                        [0, 2] => [1, false],
+    #                                                        [1, -1] => [1, false],
+    #                                                        [1, 0] => [2, false],
+    #                                                        [1, 1] => [1, true],
+    #                                                        [1, 2] => [1, false],
+    #                                                        [2, -1] => [0, false],
+    #                                                        [2, 0] => [1, false],
+    #                                                        [2, 1] => [1, false],
+    #                                                        [2, 2] => [1, false])
+    # end
   end
 end
