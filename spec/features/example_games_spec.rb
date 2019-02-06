b4cd872eae0ab391121d711a974ca52eca0ffd34
matharotheelf <mrthomasjacoblawrence@hotmail.co.uk>
2019-02-06 19:01:57 +0000

    Added feature test for scenario 6 seeded grid

--- spec/features/example_games_spec.rb
+++ spec/features/example_games_spec.rb
@@ -1,10 +1,29 @@
 require 'game.rb'
 
 describe 'User journeys' do
-  it 'empty game grid will remain empty' do
-    game = Game.new
-    grid_zero = Grid.new
-    grid_one = game.iterate(grid_zero)
+  before do
+    @game = Game.new
+    @grid_zero = Grid.new
+  end
+
+  it 'scenario 5: empty game grid will remain empty' do
+    grid_one = @game.iterate(@grid_zero)
     expect(grid_one.livecells_array).to eq([])
+  end
+
+  it 'scenario 6: seeded grid will oscillate' do
+    @grid_zero.add_livecell(-1, 0)
+    @grid_zero.add_livecell(0, 0)
+    @grid_zero.add_livecell(1, 0)
+    grid_one = @game.iterate(@grid_zero)
+    livecellarrayone = [grid_one.livecells_array[0].coordinate,
+                        grid_one.livecells_array[1].coordinate,
+                        grid_one.livecells_array[2].coordinate]
+    expect(livecellarrayone).to include([0, -1], [0, 0], [0, 1])
+    grid_two = @game.iterate(grid_one)
+    livecellarraytwo = [grid_two.livecells_array[0].coordinate,
+                        grid_two.livecells_array[1].coordinate,
+                        grid_two.livecells_array[2].coordinate]
+    expect(livecellarraytwo).to include([-1, 0], [0, 0], [1, 0])
   end
 end
